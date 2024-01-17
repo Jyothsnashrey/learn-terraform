c56971746adfe0702d05d2ffa26e1cd3d050bde3
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-01-17 16:12:44 -0600

    added terraform code

---
+++ expense-v2/variables.tf
@@ -0,0 +1,12 @@
+
+variable "instance_type"{
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-07d06a948af69411d"]
+}
+
+variable "zone_id" {
+  default = Z0280752N15KXNCY0H6Y
+}
